0faa3dc37c031b61c1250364b069bea114887b5f
Inder Kumar Rathore <inder.kumar@aspiringminds.in>
2018-09-07 11:30:28 +0530

    Added keyboard handling for table view

--- TMDb/Src/Scenes/SearchScene/SearchViewController.swift
+++ TMDb/Src/Scenes/SearchScene/SearchViewController.swift
@@ -45,6 +45,18 @@ class SearchViewController: UIViewController {
   override func viewDidLoad() {
     super.viewDidLoad()
   }
+  
+  override func viewWillAppear(_ animated: Bool) {
+    super.viewWillAppear(animated)
+    // Register keyboard notifications
+    registerForKeyboardNotifications()
+  }
+  
+  override func viewDidDisappear(_ animated: Bool) {
+    super.viewDidDisappear(animated)
+    // Register keyboard notifications
+    deregisterForKeyboardNotifications()
+  }
 }
 
 // MARK:- SearchDisplayLogic
@@ -80,5 +92,48 @@ extension SearchViewController: UITableViewDataSource {
 }
 
 
+// MARK:- KeyboardHandling
+extension SearchViewController {
+  /// Registers for Keyboard notifications
+  private func registerForKeyboardNotifications() {
+    NotificationCenter.default.addObserver(self, selector: #selector(keyboardWasShown(notification:)), name: .UIKeyboardDidShow, object: nil)
+    
+    NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillBeHidden(notification:)), name: .UIKeyboardWillHide, object: nil)
+  }
+  
+  /// De-Registers for Keyboard notifications
+  private func deregisterForKeyboardNotifications() {
+    NotificationCenter.default.removeObserver(self, name: .UIKeyboardDidShow, object: nil)
+    NotificationCenter.default.removeObserver(self, name: .UIKeyboardWillHide, object: nil)
+  }
+  
+  // Called when the UIKeyboardDidShowNotification is sent.
+  @objc func keyboardWasShown(notification: Notification) {
+    if let info = notification.userInfo as? [String: AnyObject] {
+      if let kbValue = info[UIKeyboardFrameEndUserInfoKey] as? NSValue {
+        // KeyboardSize
+        let kbSize = kbValue.cgRectValue.size
+        // Create the insents for scroll view
+        let contentInsets = UIEdgeInsets(top: 0.0, left: 0.0, bottom: kbSize.height, right: 0.0)
+        self.tableView.contentInset = contentInsets
+        self.tableView.scrollIndicatorInsets = contentInsets
+        
+//        // If active text field is hidden by keyboard, scroll it so it's visible
+//        // Your app might not need or want this behavior.
+//        CGRect aRect = self.view.frame
+//        aRect.size.height -= kbSize.height
+//        if (!CGRectContainsPoint(aRect, activeField.frame.origin) ) {
+//          [self.scrollView scrollRectToVisible:activeField.frame animated:YES]
+//        }
+      }
+    }}
+  
+  // Called when the UIKeyboardWillHideNotification is sent
+  @objc func keyboardWillBeHidden(notification: Notification) {
+    let contentInsets = UIEdgeInsets.zero
+    self.tableView.contentInset = contentInsets
+    self.tableView.scrollIndicatorInsets = contentInsets
+  }
+}
 
 
